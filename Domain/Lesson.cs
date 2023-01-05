5fb96872e7ab05e9897c6a6df0438091e579fa91
Anatoly <90443893+Carineitor77@users.noreply.github.com>
2023-01-05 18:00:14 +0200

    Added basic entities

---
+++ Domain/Lesson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Lesson : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public long UserId { get; set; }
+        public User? User { get; set; }
+
+        public ICollection<Visiting>? Visitings { get; set; }
+    }
+}
---
+++ Domain/User.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Domain
+{
+    public class User : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public byte[]? PasswordHash { get; set; }
+        public byte[]? PasswordSalt { get; set; }
+
+        public long RoleId { get; set; }
+        public Role? Role { get; set; }
+
+        public ICollection<Lesson>? Lessons { get; set; }
+
+        public ICollection<Visiting>? Visitings { get; set; }
+    }
+}
